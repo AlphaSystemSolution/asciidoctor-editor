43352e1d290354db93a115fe69d30f00cfc7c56a
sfali23 <f.syed.ali@gmail.com>
2017-02-27 12:17:59 -0500

    updated font and style with highlight

--- src/main/java/com/alphasystem/docbook/handler/impl/ArabicHandler.java
+++ src/main/java/com/alphasystem/docbook/handler/impl/ArabicHandler.java
@@ -11,11 +11,12 @@ import static com.alphasystem.openxml.builder.wml.WmlBuilderFactory.getRFontsBui
  */
 abstract class ArabicHandler implements InlineStyleHandler {
 
-    private static final String ARABIC_FONT_NAME = "Arabic Typesetting";
-    private static final long DEFAULT_SIZE = 36;
+    private static final String ARABIC_FONT_NAME = "KFGQPC Uthman Taha Naskh";
+    private static final long DEFAULT_SIZE = 28;
     static final String ARABIC_HEADING_1 = "arabicHeading1";
     static final String ARABIC_TABLE_CAPTION = "arabicTableCaption";
     static final String ARABIC_NORMAL = "arabicNormal";
+    static final String ARABIC_NORMAL_HIGHLIGHT = "arabicNormalHighlight";
 
     private final long size;
 
